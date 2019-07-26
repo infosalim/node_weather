c46ebe487eb326741e0e05af4206777deac43ccb
infosalim <salimh5757@gmail.com>
2019-07-26 14:48:42 +0600

    got api response

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const request = require('request');
-const url = require('https://api.darksky.net/forecast/6845af35ca447652f3eae9489395c091/37.8267,-122.4233');
+const url = 'https://api.darksky.net/forecast/6845af35ca447652f3eae9489395c091/37.8267,-122.4233';
+
+
+request({ url: url }, (error, response)=>{
+    const data = JSON.parse(response.body);
+    console.log(data);
+});
