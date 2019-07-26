6fb4a236004f31b9989b622e4073d9bbedd5df79
infosalim <salimh5757@gmail.com>
2019-07-27 01:00:25 +0600

    worked on geolocation API

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const url = 'https://api.darksky.net/forecast/6845af35ca447652f3eae9489395c091/3
 request({ url: url, json: true }, (error, response)=>{
     console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
 });
+
+
+const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGVyZWlzc2FsaW0iLCJhIjoiY2p5a2c3dGs2MGU3MDNtcnU0czl4dG5tMSJ9.W5LkJC1d1IlJ2PDz-i2JCw';
+
+request({ url: geoCodeURL, json: true}, (error, response)=>{
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+});
