bc35efc9278499a10257d44bd720e7da864515ec
infosalim <salimh5757@gmail.com>
2019-07-27 00:30:04 +0600

    customize some HTTP request

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/6845af35ca447652f3eae9489395c091/37.8267,-122.4233';
 
 
-request({ url: url }, (error, response)=>{
-    const data = JSON.parse(response.body);
-    console.log(data);
+request({ url: url, json: true }, (error, response)=>{
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
 });
