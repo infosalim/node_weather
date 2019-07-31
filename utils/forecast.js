4f02f95a4460cad20bdcd671b35d2356f9a467e1
infosalim <salimh5757@gmail.com>
2019-08-01 01:54:38 +0600

    used distructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/6845af35ca447652f3eae9489395c091/${latitude}, ${longitude}`;
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect weather service!', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to connect location.', undefined);
         } else {
-            callback(undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
         }
     });
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request');
 const geocode = (address, callback)=>{
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiaGVyZWlzc2FsaW0iLCJhIjoiY2p5a2c3dGs2MGU3MDNtcnU0czl4dG5tMSJ9.W5LkJC1d1IlJ2PDz-i2JCw`;
 
-    request({url: url, json: true}, (error, response)=>{
+    request({url, json: true}, (error, { body })=>{
         if(error){
             callback('Unable to connect to location service!', undefined);
-        }else if(response.body.features.length === 0){
+        }else if(body.features.length === 0){
             callback('Unable to find Location. Try another search.', undefined);
         }else{
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
